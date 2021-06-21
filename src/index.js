62fa0f1779a203d68ea26ff1c34a0db4191d7932
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-06-22 01:42:30 +0300

    init layout

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const C = document.querySelector("canvas"),
-  $ = C.getContext("2d"),
-  W = (C.width = window.innerWidth),
-  H = (C.height = window.innerHeight - 1);
+const canvas = document.querySelector("canvas");
+const width = (canvas.width = 400);
+const height = (canvas.height = 350);
+const ctx = canvas.getContext("2d");
 
-const str = "React Vue Next Nuxt GraphQL ES6+",
-  matrix = str.split("");
-let font = 15,
-  col = W / font,
-  arr = [];
+const sourceString = "React Vue Next Nuxt GraphQL ES6+";
+const matrixSymbols = sourceString.split("");
+const fontSize = 15;
+let columns = width / fontSize;
+let arrayModel = [];
 
-for (let i = 0; i < col; i++) arr[i] = 1;
+const interval = 80;
+
+for (let i = 0; i < columns; i++) arrayModel[i] = 1;
 let currentSymbolIndex = 0;  
 
 function draw() {
-  $.fillStyle = "rgba(0,0,0,.05)";
-  $.fillRect(0, 0, W, H);
-  $.fillStyle = "#0f0";
-  $.font = font + "px system-ui";
-  for (let i = 0; i < arr.length; i++) {
-    let txt = matrix[currentSymbolIndex < matrix.length ? currentSymbolIndex : currentSymbolIndex % matrix.length];
-    $.fillText(txt, i * font, arr[i] * font);
-    if (arr[i] * font > H && Math.random() > 0.975) arr[i] = 0;
-    arr[i]++;
+  ctx.fillStyle = "rgba(0, 0, 0,.05)";
+  ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = "#0f0";
+  ctx.font = fontSize + "px system-ui";
+  for (let i = 0; i < arrayModel.length; i++) {
+    let txt = matrixSymbols[currentSymbolIndex < matrixSymbols.length ? currentSymbolIndex : currentSymbolIndex % matrixSymbols.length];
+    ctx.fillText(txt, i * fontSize, arrayModel[i] * fontSize);
+    if (arrayModel[i] * fontSize > height && Math.random() > 0.975) arrayModel[i] = 0;
+    arrayModel[i]++;
     currentSymbolIndex++;
   }
 }
 
-setInterval(draw, 80);
-
-window.addEventListener("resize", () => location.reload());
+setInterval(draw, interval);
